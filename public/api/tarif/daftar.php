67639ff79e18a9d576bbd9c7c3665e3ec9856954
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-03 22:03:47 +0700

    Ambil daftar tarif

---
+++ public/api/tarif/daftar.php
@@ -0,0 +1,44 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] != "GET") {
+        http_response_code(400);
+        die();
+    }
+    
+    session_start();
+
+    $input_data = json_decode(file_get_contents("php://input"), true);
+    $conn = new mysqli("dbparkir.my.id", "hanif", "123", "parkirin");
+
+    if ($conn->connect_error) {
+        http_response_code(500);
+        die();
+    }
+
+    if (empty($_SESSION["uuid"]) && empty($_SESSION["level"])) {
+        http_response_code(401);
+        die();
+    }
+
+    if ($_SESSION["level"] != "Admin") {
+        http_response_code(403);
+        die();
+    }
+
+    $sql    = "SELECT id, jenis from tarif";
+    $result = $conn->query($sql);
+    $data   = array();
+
+    if ($result) {
+        while($row = $result->fetch_assoc()) {
+            $data[] = array(
+                "id"    => $row["id"],
+                "jenis" => $row["jenis"]
+            );
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode($data, JSON_PRETTY_PRINT);
+    } else {
+        http_response_code(500);
+    }
+?>
