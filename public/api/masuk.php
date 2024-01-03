d1be3f264c07676864f55abeafaf05433444c6f1
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-03 20:16:02 +0700

    Merapihkan kode

--- public/api/masuk.php
+++ public/api/masuk.php
@@ -9,13 +9,13 @@
         die();
     }
 
-    if (empty($input_data['username']) || empty($input_data['password'])) {
+    if (empty($input_data["username"]) || empty($input_data["password"])) {
         http_response_code(400);
         die();
     }
 
-    $input_username = $conn->real_escape_string($input_data['username']);
-    $input_password = $conn->real_escape_string($input_data['password']);
+    $input_username = $conn->real_escape_string($input_data["username"]);
+    $input_password = $conn->real_escape_string($input_data["password"]);
 
     $sql = "SELECT uuid, level FROM pegawai WHERE id = '$input_username' AND sandi = MD5('$input_password')";
     $result = $conn->query($sql);
@@ -23,8 +23,8 @@
     if ($result) {
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
-            $_SESSION['uuid'] = $row["uuid"];
-            $_SESSION['level'] = $row["level"];
+            $_SESSION["uuid"]  = $row["uuid"];
+            $_SESSION["level"] = $row["level"];
         } else {
             http_response_code(404);
         }
