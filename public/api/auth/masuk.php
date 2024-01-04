e39b7388797626bf155a9384170a7bb50a29df52
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2024-01-04 15:32:02 +0700

    Cors masuk (#4)
    * API masuk selesai
    
    * API keluar seslai
    
    * Merapihkan kode
    
    * Ambil daftar pegawai
    
    * validasi tipe request
    
    * Merapihkan
    
    * Perbaikan " -> '
    
    * Persingkat
    
    * Perbaikan dikit
    
    * Mengambil info pegawai
    
    * Anti injeksi
    
    * Status server
    
    * Tambah pegawai
    
    * Ubah struktur
    
    * Berubah dikit gak ngaruh
    
    * Edit pegawai
    
    * Ambil daftar tarif
    
    * Ambil info berdasarkan id
    
    * Ubah tipe data
    
    * Hapus tarif berdasarkan id
    
    * Tambah tarif baru
    
    * Edit tarif berdasarkan id
    
    * Pemarkiran
    
    * Masuk
    
    ---------
    
    Co-authored-by: dodidev <37464431+dodichakill@users.noreply.github.com>

--- public/api/auth/masuk.php
+++ public/api/auth/masuk.php
@@ -1,4 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Content-Type");
     $input_data = json_decode(file_get_contents("php://input"), true);
     
     if ($_SERVER["REQUEST_METHOD"] != "POST" || empty($input_data["username"]) || empty($input_data["password"])) {
