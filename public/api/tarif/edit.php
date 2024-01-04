269c076ba90164c7d9d818a8b3f250259966fc04
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-04 16:44:56 +0700

    Cors temenan

--- public/api/tarif/edit.php
+++ public/api/tarif/edit.php
@@ -7,7 +7,7 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, PATCH, OPTIONS");
+            header("Access-Control-Allow-Methods: PATCH, OPTIONS");
     
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
--- public/api/tarif/hapus.php
+++ public/api/tarif/hapus.php
@@ -7,7 +7,7 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            header("Access-Control-Allow-Methods: DELETE, OPTIONS");
     
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
