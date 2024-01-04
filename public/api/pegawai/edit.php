9bb83989f86844fbba65fe195ac741a6b897f1f8
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-04 19:49:38 +0700

    Jenis Kelamin -> gender

--- public/api/pegawai/edit.php
+++ public/api/pegawai/edit.php
@@ -16,7 +16,7 @@
     }
 
     $input_data = json_decode(file_get_contents("php://input"), true);
-    if ($_SERVER["REQUEST_METHOD"] != "PATCH" || empty($_GET["uuid"]) || empty($input_data["username"]) || empty($input_data["password"]) || empty($input_data["nama"]) || empty($input_data["jenis_kelamin"]) || empty($input_data["no_telp"]) || empty($input_data["alamat"]) || empty($input_data["level"])) {
+    if ($_SERVER["REQUEST_METHOD"] != "PATCH" || empty($_GET["uuid"]) || empty($input_data["username"]) || empty($input_data["password"]) || empty($input_data["nama"]) || empty($input_data["gender"]) || empty($input_data["no_telp"]) || empty($input_data["alamat"]) || empty($input_data["level"])) {
         http_response_code(400);
         die();
     }
@@ -45,7 +45,7 @@
     $id      = $conn->real_escape_string($input_data["username"]);
     $sandi   = $conn->real_escape_string($input_data["password"]);
     $nama    = $conn->real_escape_string($input_data["nama"]);
-    $kelamin = $conn->real_escape_string($input_data["jenis_kelamin"]);
+    $kelamin = $conn->real_escape_string($input_data["gender"]);
     $telp    = $conn->real_escape_string($input_data["no_telp"]);
     $alamat  = $conn->real_escape_string($input_data["alamat"]);
     $level   = $conn->real_escape_string($input_data["level"]);
