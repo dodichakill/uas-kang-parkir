d62dd83eaec0f9f70f7f724131f2bf0924eb75ba
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-04 17:18:12 +0700

    Salah method

--- public/api/kendaraan-masuk/parkir.php
+++ public/api/kendaraan-masuk/parkir.php
@@ -7,7 +7,7 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, OPTIONS");
+            header("Access-Control-Allow-Methods: POST, OPTIONS");
     
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
