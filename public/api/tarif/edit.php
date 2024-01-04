74fe17c9f79edb0ff2bca8696de98187ed891f37
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-01-04 15:57:05 +0700

    Tarif Cors

--- public/api/tarif/edit.php
+++ public/api/tarif/edit.php
@@ -1,4 +1,14 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: PATCH, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Max-Age: 3600");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
+
     $input_data = json_decode(file_get_contents("php://input"), true);
     if ($_SERVER["REQUEST_METHOD"] != "PATCH" || empty($_GET["id"]) || empty($input_data["jenis"]) || empty($input_data["waktu"]) || empty($input_data["biaya_normal"]) || empty($input_data["biaya_perjam"])) {
         http_response_code(400);
